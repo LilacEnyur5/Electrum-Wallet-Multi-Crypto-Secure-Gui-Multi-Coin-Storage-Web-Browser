99800e366d65aec272d33d435edce3dd995d8df1
JJnr83x <hf2498dfjhncds@outlook.com>
2024-08-13 18:02:16 +0300

    Upload Files

---
+++ Electrum-Wallet/packages/.NetSDK.UnitTest/Currency/CurrencyTest.cs
@@ -0,0 +1,37 @@
+﻿using Coinbase.Currecny;
+using Coinbase.Wallet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coinbase.NetSDK.UnitTest.Currency
+{
+    [TestClass]
+    public class CurrencyTest : TestEssential
+    {
+        [TestMethod]
+        public void GetBuyPriceTest()
+        {
+            var price = client.GetBuyPrice("BTC-USD");
+
+            Assert.IsNotNull(price);
+        }
+
+        [TestMethod]
+        public void GetSellPriceTest()
+        {
+            var price = client.GetSellPrice("BTC-USD");
+
+            Assert.IsNotNull(price);
+        }
+
+        [TestMethod]
+        public void GetSpotPriceTest()
+        {
+            var price = client.GetSpotPrice("BTC-USD");
+
+            Assert.IsNotNull(price);
+        }
+    }
+}
